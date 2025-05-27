3060b706be7db7ebdc3f753409b1bf06b2a12dc7
JecyNside <Edu_Cordova1@hotmail.com>
2025-05-27 04:03:56 -0500

    fix(project): added scrolltrigger animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,38 @@ import Background2 from "./assets/imgs/background-2.jpg";
 import Background3 from "./assets/imgs/background-3.jpg";
 import ReactLenis from "lenis/react";
 
+gsap.registerPlugin(ScrollTrigger);
+
 function App() {
+
   useGSAP(() => {
-    gsap.to(".section-2", {});
+    gsap.to(".section-1", {
+      scale: 0.8,
+      rotate: -6,
+      scrollTrigger: {
+        trigger: ".section-2",
+        start: "top 100%",
+        end: "bottom 100%",
+        scrub: true,
+      },
+    });
+
+    gsap.to(".section-2", {
+      scale: 0.8,
+      rotate: -6,
+      scrollTrigger: {
+        trigger: ".section-3",
+        start: "top 100%",
+        end: "bottom 100%",
+        scrub: true,
+      },
+    });
   }, {});
 
   return (
     <ReactLenis root>
       <main>
-        <section>
+        <section className="section-1">
           <img src={Background1} />
         </section>
 
@@ -22,7 +45,7 @@ function App() {
           <img src={Background2} />
         </section>
 
-        <section className="section-2">
+        <section className="section-3">
           <img src={Background3} />
         </section>
       </main>
